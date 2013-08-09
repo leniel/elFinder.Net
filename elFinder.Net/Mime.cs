c490de9edb7abeeac526a3d8ce54003feff528d7
Bjrn Sllarp <bjorn.sallarp@valtech.se>
2013-08-09 15:51:58 +0200

    Fixed problem with loading mime types from the assembly

--- elFinder.Net/Mime.cs
+++ elFinder.Net/Mime.cs
@@ -4,37 +4,42 @@ using System.Reflection;
 
 namespace ElFinder
 {
+    using System;
+
     internal static class Mime
     {
         private static Dictionary<string, string> _mimeTypes;
+        
         static Mime()
         {
             _mimeTypes = new Dictionary<string, string>();
             Assembly assembly = Assembly.GetExecutingAssembly();
-            if(assembly != null)
+
+            using (var stream = assembly.GetManifestResourceStream("elFinder.Net.mimeTypes.txt"))
             {
-                using (Stream stream = assembly.GetManifestResourceStream("ElFinder.mimeTypes.txt"))
+                using (var reader = new StreamReader(stream))
                 {
-                    using (StreamReader reader = new StreamReader(stream))
+                    while (!reader.EndOfStream)
                     {
-                        while (!reader.EndOfStream)
+                        string line = reader.ReadLine();
+
+                        if (string.IsNullOrEmpty(line) || line.StartsWith("#"))
                         {
-                            string line = reader.ReadLine();
-                            line = line.Trim();
-                            if (!string.IsNullOrWhiteSpace(line) && line[0] != '#')
+                            continue;
+                        }
+
+                        var parts = line.Trim().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                        if (parts.Length > 1)
+                        {
+                            var mime = parts[0];
+
+                            for (var i = 1; i < parts.Length; i++)
                             {
-                                string[] parts = line.Split(' ');
-                                if (parts.Length > 1)
+                                var ext = parts[i].ToLower();
+                                if (!_mimeTypes.ContainsKey(ext))
                                 {
-                                    string mime = parts[0];
-                                    for (int i = 1; i < parts.Length; i++)
-                                    {
-                                        string ext = parts[i].ToLower();
-                                        if (!_mimeTypes.ContainsKey(ext))
-                                        {
-                                            _mimeTypes.Add(ext, mime);
-                                        }
-                                    }
+                                    _mimeTypes.Add(ext, mime);
                                 }
                             }
                         }
@@ -42,16 +47,15 @@ namespace ElFinder
                 }
             }
         }
+
         public static string GetMimeType(string extension)
         {
             if (_mimeTypes.ContainsKey(extension))
             {
                 return _mimeTypes[extension];
             }
-            else
-            {
-                return "unknown";
-            }                    
+            
+            return "unknown";
         }
     }
 }
