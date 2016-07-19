0c4928290af5b1a58acdaac3756e1c9fa7683f11
Nghiahd <ducnghiatq@gmail.com>
2016-07-19 23:28:29 +0700

    Out of memory bug fixed (because some invalid image path). Check image dimension after upload, make standard file name after upload

---
+++ elFinder.Net/Facade/StringExtension.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+
+namespace ElFinder
+{
+    using System;
+    using System.Diagnostics;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Web;
+    using System.Collections.Generic;
+
+    public static class StringExtension
+    {        
+        private const string InvalidFileNameChars = "[ .,_/\\\\+:;?!@]|<>'()^`";       
+
+        public static string Transliterate(this string text, bool allowunicodeCharacters = false)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return string.Empty;
+            }
+            var rgx = new Regex(InvalidFileNameChars);
+            text = rgx.Replace(text, "-").Trim();
+            return Utf8ToAsciiConverter.ToAsciiString(text).ToLower();            
+        }                     
+    }
+}
